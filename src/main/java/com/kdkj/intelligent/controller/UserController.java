37dcdfa8160d8839deb41359a8fe1162b501cc45
zxc9224 <307231881@qq.com>
2018-01-05 18:47:27 +0800

    usercontroller新增方法

--- src/main/java/com/kdkj/intelligent/controller/UserController.java
+++ src/main/java/com/kdkj/intelligent/controller/UserController.java
@@ -1,8 +1,7 @@
 package com.kdkj.intelligent.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,11 +18,26 @@ public class UserController {
     @Autowired
     private UsersService usersService;
 
-    @RequestMapping(value = "/selectListByUser", method = RequestMethod.GET)
-    public Result selectListByUser(Users record) {
-    	
+    @RequestMapping(value = "/selectListByUser", method = RequestMethod.POST)
+    public Result selectListByUser(HttpRequest request,Users record) {
     	PageHelper.startPage(record.getCurrent(), record.getPageSize());
     	PageInfo<Users> page=usersService.selectListByUser(record);
 		return Result.ok("查询成功", page);
     }
+    
+    @RequestMapping(value = "/selectById", method = RequestMethod.GET)
+    public Result selectById(HttpRequest request,int id) {
+    	Users user=usersService.selectByPrimaryKey(id);
+		return Result.ok("查询成功", user);
+    }
+    
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    public Result update(HttpRequest request,Users record) {
+    	usersService.updateByPrimaryKey(record);
+		return Result.ok("修改成功");
+    }
+    
+    
+    
+    
 }
