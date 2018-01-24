34317f6a6cee247ff050a031e5856dfb6549a623
zxc9224 <307231881@qq.com>
2018-01-24 18:43:39 +0800

    usercontroller

--- src/main/java/com/kdkj/intelligent/controller/UserController.java
+++ src/main/java/com/kdkj/intelligent/controller/UserController.java
@@ -6,11 +6,13 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
-import com.alibaba.fastjson.JSON;
-import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
@@ -35,7 +37,7 @@ public class UserController {
 	public Result selectListByUser(HttpServletRequest request, @RequestBody Users record) {
 		PageHelper.startPage(record.getCurrent(), record.getPageSize());
 		List<Users> list = usersService.selectListByUser(record);
-		PageInfo<Users> page = new PageInfo(list);
+		PageInfo<Users> page = new PageInfo<Users>(list);
 		return Result.ok("查询成功", page);
 	}
 
